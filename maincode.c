86bbe0d6ac97651cb1feb803926cd30ec7f166fe
dimacuhamarc <66410761+dimacuhamarc@users.noreply.github.com>
2021-01-20 12:33:53 +0800

    Add files via upload

--- maincode.c
+++ maincode.c
@@ -6,12 +6,24 @@
 
 //global variables
 FILE *cRec; //file
-char lastName[20];
-char givenName[20];
-char gender[10]; //student names
-int cNum,g1,g2,g3,g4; //grades
 int caseMenu; //casemenu
+struct record
+{
+    char lastName[20];
+    char givenName[20];
+    char gender[10]; //student names
+    int cNum,g1,g2,g3,g4; //grades
 
+};
+
+void redeClassRecord();
+void readClassRecord();
+
+void readClassRecord()
+{
+    struct record std;
+    cRec = fopen("ClassRecord.txt","a+");
+}
 void main()
 {
     // Variables
@@ -39,7 +51,7 @@ void main()
         switch(sMenu)
         {
         case 1: addStudent(); break;
-        case 2: printf("\t\t\t-=-=-=- Edit Student -=-=-=-\n"); break;
+        case 2: editStudent(); printf("\t\t\t-=-=-=- Edit Student -=-=-=-\n"); break;
         case 3: deleteStudent();printf("\t\t\t-=-=-=- Delete Student -=-=-=-\n"); break;
         case 4: displayStudent(); break;
         case 5: displayAllStudents(); break;
@@ -57,7 +69,7 @@ void deleteStudent()
 void addStudent()
 {
     system("cls");
-
+    struct record std;
 
     char tempSurname[20];
     char tempGiven[20];
@@ -66,7 +78,7 @@ void addStudent()
 
     printf("\t\t-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-\n");
     readClassRecord();
-    while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&cNum,&gender,&givenName,&lastName,&g1,&g2,&g3,&g4)!=EOF){}
+    while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&std.cNum,&std.gender,&std.givenName,&std.lastName,&std.g1,&std.g2,&std.g3,&std.g4)!=EOF){}
     fclose(cRec);
 
 
@@ -89,14 +101,16 @@ void addStudent()
     printf("\n\t\t            FOURTH      : ");
     scanf("%d",&tg4);
 
-    tempcNum = cNum + 1;
+    tempcNum = std.cNum + 1;
 
     fprintf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",tempcNum,tempGender,tempGiven,tempSurname,tg1,tg2,tg3,tg4);
+    fclose(cRec);
     system("cls");
 
 }
 void displayStudent()
 {
+    struct record std;
     char tempSurname[20];
     do
     {
@@ -106,11 +120,19 @@ void displayStudent()
         readClassRecord();
         printf("\t\t\tEnter Student Surname: ");
         scanf("%s",tempSurname);
-        while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&cNum,&gender,&givenName,&lastName,&g1,&g2,&g3,&g4)!=EOF)
+        while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&std.cNum,&std.gender,&std.givenName,&std.lastName,&std.g1,&std.g2,&std.g3,&std.g4)!=EOF)
         {
-            if(strcmp(lastName,tempSurname) == 0)
+            if(strcmp(std.lastName,tempSurname) == 0)
             {
-                printf("\n\t\t\t-=-=-=- INFORMATION -=-=-=-\n\t\t ID NUMBER: %d \n\t\t Last Name: %s \n\t\t First Name:  %s\n\n\t\t\t-=-=-=- GRADES -=-=-=-\n\t\t First Quarter: %d \n\t\t Second Quarter: %d\n\t\t Third Quarter: %d\n\t\t Fourth Quarter: %d\n\n",cNum,lastName,givenName,g1,g2,g3,g4);
+                printf("\n\t\t\t-=-=-=- INFORMATION -=-=-=-\n");
+                printf("\t\tID NUMBER: %d \n", std.cNum);
+                printf("\t\tLast Name: %s \n", std.lastName);
+                printf("\t\tFirst Name: %s \n", std.givenName);
+                printf("\n\t\t\t-=-=-=- GRADES -=-=-=-\n");
+                printf("\t\tFirst Quarter: %d \n", std.g1);
+                printf("\t\tSecond Quarter: %d \n", std.g2);
+                printf("\t\tThird Quarter: %d \n", std.g3);
+                printf("\t\tFourth Quarter: %d \n", std.g4);
                 break;
             }
             else
@@ -138,13 +160,22 @@ void displayStudent()
 }
 void displayAllStudents()
 {
+    struct record std;
     system("cls");
     printf("\t\t-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-\n");
     printf("\t\t\t-=-=-=- Student Record -=-=-=-\n");
     readClassRecord();
-    while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&cNum,&gender,&givenName,&lastName,&g1,&g2,&g3,&g4)!=EOF)
+    while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&std.cNum,&std.gender,&std.givenName,&std.lastName,&std.g1,&std.g2,&std.g3,&std.g4)!=EOF)
     {
-        printf("\n\t\t\t-=-=-=- INFORMATION -=-=-=-\n\t\t ID NUMBER: %d \n\t\t Last Name: %s \n\t\t First Name:  %s\n\n\t\t\t-=-=-=- GRADES -=-=-=-\n\t\t First Quarter: %d \n\t\t Second Quarter: %d\n\t\t Third Quarter: %d\n\t\t Fourth Quarter: %d\n\n",cNum,lastName,givenName,g1,g2,g3,g4);
+        printf("\n\t\t\t-=-=-=- INFORMATION -=-=-=-\n");
+        printf("\t\tID NUMBER: %d \n", std.cNum);
+        printf("\t\tLast Name: %s \n", std.lastName);
+        printf("\t\tFirst Name: %s \n", std.givenName);
+        printf("\n\t\t\t-=-=-=- GRADES -=-=-=-\n");
+        printf("\t\tFirst Quarter: %d \n", std.g1);
+        printf("\t\tSecond Quarter: %d \n", std.g2);
+        printf("\t\tThird Quarter: %d \n", std.g3);
+        printf("\t\tFourth Quarter: %d \n", std.g4);
     }
     fclose(cRec);
     printf("\t\t-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-\n");
@@ -163,7 +194,62 @@ void displayAllStudents()
     return 0;
 }
 
-void readClassRecord()
+void editStudent()
 {
-    cRec = fopen("ClassRecord.txt","a+");
+    FILE *cRec;
+    char tempSurname[20];
+    struct record std;
+    system("cls");
+
+    printf("========================================\n");
+    printf("=                 EDIT                 =\n");
+    printf("========================================\n\n\n");
+
+    printf("\nEnter ID Number of student to edit: ");
+    fflush(stdin);
+    gets(tempSurname);
+
+    cRec = fopen("ClassRecord.txt","rb+");
+    if(cRec == NULL)
+        {
+        printf("\nError opening file.");
+        exit(1);
+        }
+    rewind(cRec);
+    fflush(stdin);
+
+    while(fscanf(cRec,"%d\t%s\t%s\t%s\t\t%d\t%d\t%d\t%d\n",&std.cNum,&std.gender,&std.givenName,&std.lastName,&std.g1,&std.g2,&std.g3,&std.g4)!=EOF)
+    {
+        if(strcmp(tempSurname,std.lastName) == 0)
+        {
+            printf("\t\t       -=-=-=- FILL UP INFORMATION -=-=-=-\n\n");
+            printf("\t\t            -=- BASIC INFORMATION -=-\n");
+            printf("\n\t\t            NEW Name         : ");
+            scanf("%s",&std.givenName);
+            fflush(stdin);
+            printf("\n\t\t            NEW Surname      : ");
+            scanf("%s",&std.lastName);
+            printf("\n\t\t            NEW Gender M or F: ");
+            scanf("%s",&std.gender);
+            printf("\n\t\t            -=- QUARTERLY GRADES -=-\n");
+            printf("\n\t\t            NEW FIRST        : ");
+            scanf("%d",&std.g1);
+            printf("\n\t\t            NEW SECOND       : ");
+            scanf("%d",&std.g2);
+            printf("\n\t\t            NEW THIRD        : ");
+            scanf("%d",&std.g3);
+            printf("\n\t\t            NEW FOURTH      : ");
+            scanf("%d",&std.g4);
+            fseek(cRec,-sizeof(std),SEEK_CUR);
+            fwrite(&std,sizeof(std),1,cRec);
+            break;
+        }
+    fclose(cRec);
+    printf("\n\nPress any key to continue.");
+    getch();
+    }
 }
+
+
+
+
